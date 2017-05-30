1d880a0960e1c977d12679bfeae2fe243b3f3e18
Pedagang Amanah <pedagang.amanah2016@gmail.com>
2017-05-30 14:12:46 +0700

    perbaikan icon github

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ php generator.php
 NOW=$(date +%Y-%m-%d-%H-%M-%S)
 read -p "Commit Message: " -i "Update $NOW" -e COMMIT
 git add -A
-git commit -m $COMMIT
+git commit -m "$COMMIT"
 git push origin master
