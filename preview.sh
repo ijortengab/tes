ecf31b7f8771903eaf8d2b664ea0d63aa21b4400
Pedagang Amanah <pedagang.amanah2016@gmail.com>
2017-05-31 14:03:33 +0700

    menambah effect saat ganti page

--- preview.sh
+++ preview.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 php generator.php
+if [ -f error.log ]; then
+   echo "Error found. Process Terminated."
+   cat error.log
+   rm error.log
+   exit;
+fi
 rm -rf /var/www/html/penipu.ragnarok.id
 cp -rf docs /var/www/html/penipu.ragnarok.id
